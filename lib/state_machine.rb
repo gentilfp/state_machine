f91cbc5861c20125adc4d7bda0b4172862af4956
Felipe Gentil <cdigentil@gmail.com>
2020-11-24 22:37:57 -0300

    setup initial state from machine or class initialization

--- lib/state_machine.rb
+++ lib/state_machine.rb
@@ -7,12 +7,22 @@ module StateMachine
     base.extend StateMachine::ClassMethods
   end
 
+  def initialize(initial_state = nil)
+    @initial_state = initial_state
+  end
+
+  def current_state
+    @initial_state || self.class.instance_variable_get(:@initial_state)
+  end
+
   module ClassMethods
     def state(*args)
-      name, options = args[0], args[1]
+      name, options = args[0], args[1] || {}
+
+      instance_variable_set(:@initial_state, name) if options[:initial]
 
       define_method("#{name}?") do
-        @current_state == name
+        current_state == name
       end
     end
   end
--- spec/integration/state_machine_spec.rb
+++ spec/integration/state_machine_spec.rb
@@ -4,18 +4,35 @@ RSpec.describe StateMachine do
   class StateMachineTestClass
     include StateMachine
 
-    state :standing
+    state :standing, initial: true
     state :walking
     state :running
   end
 
   context 'states' do
-    subject { StateMachineTestClass.new }
+    subject { StateMachineTestClass.new(initial_state) }
+    let(:initial_state) { nil }
 
     context 'state question (?) method definition' do
       it { expect(subject.respond_to?(:standing?)).to be_truthy }
       it { expect(subject.respond_to?(:walking?)).to be_truthy }
       it { expect(subject.respond_to?(:running?)).to be_truthy }
+    end
+
+    context 'initial and current state definition' do
+      context 'when initial state is set by state machine config' do
+        it { expect(subject.standing?).to be_truthy }
+        it { expect(subject.walking?).to be_falsey }
+        it { expect(subject.running?).to be_falsey }
+      end
+
+      context 'when initial state is set by class initialization' do
+        let(:initial_state) { :walking }
+
+        it { expect(subject.standing?).to be_falsey }
+        it { expect(subject.walking?).to be_truthy }
+        it { expect(subject.running?).to be_falsey }
+      end
     end
   end
 end
